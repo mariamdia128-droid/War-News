9fb87e478524283ab4ab98368e68d436b93f6553
najdi2025 <husseinjounier@gmail.com>
2026-09-17 12:48:44 +0300

    Use dual-trigger watchdog tasks

--- register-watchdogs.ps1
+++ register-watchdogs.ps1
@@ -2,13 +2,37 @@ $ErrorActionPreference = "Stop"
 
 $mainBat = "C:\Users\User\Desktop\lebanon-news-monitor\War-news\start-and-watch.bat"
 $devBat = "C:\Users\User\Desktop\lebanon-news-monitor\War-news-dev\start-and-watch.bat"
+$principalUser = "$env:USERDOMAIN\$env:USERNAME"
 
 # Docker Desktop usually needs an active GUI session. These tasks are registered
-# with /IT, so they run only when the user is logged on. If you choose
-# "run whether user is logged on or not" in Task Scheduler later, Docker Desktop
-# may not be available to the task.
-schtasks /Create /TN "War News Main Watchdog" /TR "`"$mainBat`"" /SC MINUTE /MO 5 /ST 00:00 /RL HIGHEST /IT /F
-schtasks /Create /TN "War News Dev Watchdog" /TR "`"$devBat`"" /SC MINUTE /MO 5 /ST 00:00 /RL HIGHEST /IT /F
+# with Interactive logon, so they run only when the user is logged on. If you
+# choose "run whether user is logged on or not" in Task Scheduler later, Docker
+# Desktop may not be available to the task.
+$principal = New-ScheduledTaskPrincipal `
+    -UserId $principalUser `
+    -LogonType Interactive `
+    -RunLevel Highest
 
-schtasks /Create /TN "War News Main Watchdog Startup" /TR "`"$mainBat`"" /SC ONSTART /RL HIGHEST /IT /F
-schtasks /Create /TN "War News Dev Watchdog Startup" /TR "`"$devBat`"" /SC ONSTART /RL HIGHEST /IT /F
+$startupTrigger = New-ScheduledTaskTrigger -AtStartup
+$repeatTrigger = New-ScheduledTaskTrigger `
+    -Once `
+    -At "00:00" `
+    -RepetitionInterval (New-TimeSpan -Minutes 5) `
+    -RepetitionDuration ([TimeSpan]::MaxValue)
+
+$mainAction = New-ScheduledTaskAction -Execute $mainBat
+$devAction = New-ScheduledTaskAction -Execute $devBat
+
+Register-ScheduledTask `
+    -TaskName "War News Main Watchdog" `
+    -Action $mainAction `
+    -Trigger @($startupTrigger, $repeatTrigger) `
+    -Principal $principal `
+    -Force
+
+Register-ScheduledTask `
+    -TaskName "War News Dev Watchdog" `
+    -Action $devAction `
+    -Trigger @($startupTrigger, $repeatTrigger) `
+    -Principal $principal `
+    -Force
